7cc2119d838a4e974f92ecf5b31a47af4bc282d9
Adam Victor Nazareth Brandizzi <adam.brandizzi@seatecnologia.com.br>
2011-03-08 00:30:13 -0300

    Cool ajax in manage page :)

--- static/manage.js
+++ static/manage.js
@@ -1,16 +1,41 @@
 function highlight_row(sender) {
-    var row = sender.parents('div#row');
+    var row = sender.parents('div.row');
     return function (data) {
         var response = $(data);
         var error = response.find('.error');
         var success = response.find('.success');
+        var ok = error.length == 0 && success != 0;
 
-        if (error.length != 0) {
-            row.addClass('error');
-        } else if (success.length != 0) {
-            row.addClass('success');
+        // Row was removed
+        if (ok && response.find('#'+row.attr('id')).length == 0) {
+            row.remove();
+            return;
         }
-   
+        // updated
+        var class_to_add = '';
+        if (error.length != 0) {
+            class_to_add = 'error';
+        } else if (success.length != 0) {
+            class_to_add = 'success';
+        }
+        row.addClass(class_to_add);
+        window.setTimeout(
+            function() {
+                row.removeClass(class_to_add);
+            },
+            500
+        );
+    }
+}
+
+function remove_row(sender) {
+    var row = sender.parents('div.row');
+    return function (data) {
+        var response = $(data);
+        var success = response.find('.success');
+
+        if (success.length != 0) {
+        }
     }
 }
 
@@ -19,6 +44,13 @@ function wtf(a, b, c) {
     alert(a);
     alert(b);
     alert(c);
+}
+
+function set_operation(sender) {
+    var submit_button = $(sender.target);
+    var form = submit_button.parents('form.form-wp');
+    var operation = $('<input id="operation" type="hidden" value="" name="' + submit_button.attr('name') + '" />');
+    form.append(operation);
 }
 
 $(document).ready(
@@ -32,8 +64,12 @@ $(document).ready(
                     success : highlight_row($(this)),
                     error : wtf
                 });
+                $(this).find('input#operation').remove();
                 return false;
             }
+        );
+        $(':submit').click(
+            set_operation
         );
     }
 );
