00738c777e0696874d8e487ad545aafbde46e1de
Adam Victor Nazareth Brandizzi <adam.brandizzi@seatecnologia.com.br>
2011-04-05 21:50:25 -0300

    Testing if wps out of a period are ignored; Contract made abstract

--- reports/models.py
+++ reports/models.py
@@ -8,9 +8,58 @@ class Contract(models.Model):
     salary = models.DecimalField(max_digits=12, decimal_places=2)
 
     def time_worked(self, start, end):
+        """
+        Returns how many hours the employee worked between the period.
+
+        Let us suppose we have the following contract:
+
+        >>> from register.tests.test_utilities import get_employee, clear_database
+        >>> from register.models import WorkingPeriod
+        >>> from datetime import datetime
+        >>> clear_database()
+        >>> employee = get_employee()
+        >>> contract = Contract(employee=employee, workload=8, salary=2000)
+
+        ...and its employee has the following working periods:
+        
+        >>> # Out the interval
+        >>> wp = WorkingPeriod(employee=employee,
+        ...         executed="test if employee has working period again",
+        ...         start=datetime(2011, 3, 31, 14, 0, 0),
+        ...         end=datetime(2011, 3, 31, 18, 0, 0))
+        >>> wp.save()
+        >>>     # in the interval
+        >>> wp = WorkingPeriod(employee=employee,
+        ...         executed="made the employe have it",
+        ...        start=datetime(2011, 4, 5, 13, 0, 0),
+        ...        end=datetime(2011, 4, 5, 18, 0, 0))
+        >>> wp.save()
+        >>> #out the interval
+        >>> wp = WorkingPeriod(employee=employee,
+        ...     executed="test if employee has working period again",
+        ...     start=datetime(2011, 5, 1, 14, 0, 0),
+        ...     end=datetime(2011, 5, 1, 18, 0, 0))
+        >>> wp.save()
+
+        If we invoke the method with a time interval which includes a period and
+        excludes others:
+
+        >>> start=datetime(2011, 4, 1)
+        >>> end=datetime(2011, 4, 30)
+
+        ...the result should be only the time of the periods included:
+
+        >>> contract.time_worked(start, end)
+        5.0
+
+        >>> clear_database()
+        """
         working_peridos = WorkingPeriod.objects.filter(
             start__gte=start, end__lte=end, employee=self.employee)
 
         time_worked = sum(period.total_time for period in working_peridos)
 
         return time_worked
+
+    class Meta:
+        abstract=True
--- reports/tests/contract.py
+++ reports/tests/contract.py
@@ -17,17 +17,12 @@ class ContractTestCase(ModelTestCase):
 
     def create_contract(self):
         contract = Contract(employee=self.employee, workload=8, salary=2000)
-        contract.save()
-
-        contract2 = Contract.objects.get(employee=self.employee)
-
-        self.assertEqual(contract, contract2)
 
     def time_worked(self):
         organization, project, task = get_organization_project_task()
 
         contract = Contract(employee=self.employee, workload=8, salary=2000)
-        contract.save()
+        #contract.save()
 
         wps = [
             WorkingPeriod(employee=self.employee,
@@ -59,11 +54,70 @@ class ContractTestCase(ModelTestCase):
         time_worked = contract.time_worked(start, end)
         self.assertEqual(time_worked, 8-4 + 18-14 + 8-4 + 18-13)
 
+    def time_worked_ignore_those_out_the_interval(self):
+        organization, project, task = get_organization_project_task()
+
+        contract = Contract(employee=self.employee, workload=8, salary=2000)
+
+        wps = [
+            # Out the interval
+            WorkingPeriod(employee=self.employee,
+                executed="made the employe have it",
+                executed_task=task,
+                start=datetime(2011, 3, 31, 8, 0, 0),
+                end=datetime(2011, 3, 31, 12, 0, 0)),
+            WorkingPeriod(employee=self.employee,
+                executed="test if employee has working period again",
+                executed_task=task,
+                start=datetime(2011, 3, 31, 14, 0, 0),
+                end=datetime(2011, 3, 31, 18, 0, 0)),
+            # in the interval
+            WorkingPeriod(employee=self.employee,
+                executed="made the employe have it",
+                executed_task=task,
+                start=datetime(2011, 4, 4, 8, 0, 0),
+                end=datetime(2011, 4, 4, 12, 0, 0)),
+            WorkingPeriod(employee=self.employee,
+                executed="test if employee has working period again",
+                executed_task=task,
+                start=datetime(2011, 4, 4, 14, 0, 0),
+                end=datetime(2011, 4, 4, 18, 0, 0)),
+            WorkingPeriod(employee=self.employee,
+                executed="made the employe have it",
+                executed_task=task,
+                start=datetime(2011, 4, 5, 8, 0, 0),
+                end=datetime(2011, 4, 5, 12, 0, 0)),
+            WorkingPeriod(employee=self.employee,
+                executed="made the employe have it",
+                executed_task=task,
+                start=datetime(2011, 4, 5, 13, 0, 0),
+                end=datetime(2011, 4, 5, 18, 0, 0)),
+            #out the interval
+            WorkingPeriod(employee=self.employee,
+                executed="made the employe have it",
+                executed_task=task,
+                start=datetime(2011, 5, 1, 8, 0, 0),
+                end=datetime(2011, 5, 1, 12, 0, 0)),
+            WorkingPeriod(employee=self.employee,
+                executed="test if employee has working period again",
+                executed_task=task,
+                start=datetime(2011, 5, 1, 14, 0, 0),
+                end=datetime(2011, 5, 1, 18, 0, 0)),
+        ]
+        for wp in wps: wp.save()
+        
+        start=datetime(2011, 4, 1)
+        end=datetime(2011, 4, 30)
+        
+        time_worked = contract.time_worked(start, end)
+        self.assertEqual(time_worked, 8-4 + 18-14 + 8-4 + 18-13)
+
     def tearDown(self):
         self.employee.delete_with_user()        
 
 contractTestSuite = unittest.TestSuite()
 contractTestSuite.addTest(ContractTestCase('create_contract'))
 contractTestSuite.addTest(ContractTestCase('time_worked'))
+contractTestSuite.addTest(ContractTestCase('time_worked_ignore_those_out_the_interval'))
 
 
