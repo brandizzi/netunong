03ba6c49e2b9ba289c0fc218dc8e4544a29fb0dd
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2012-01-12 22:37:53 -0200

    Now twill tests are runnable

--- run_twill_tests.sh
+++ run_twill_tests.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 export DJANGO_SETTINGS_MODULE=settings 
 export PYTHONPATH=. 
-. ~/Library/flunc/bin/activate
 python cleartest.py
+echo 'STARTING NETUNONG'
+python manage.py runserver 32198 > netunong.log &
+sleep 1
 echo 'RUNNING TESTS FOR APPLICATION register'
 flunc -ptwill all
+kill $(ps -opid,command | awk '/runserver 32198/{if(!/awk/)print $1}')
