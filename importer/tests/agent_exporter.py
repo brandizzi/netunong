48ef3df4b6e99fa10aae53b6a1787d6dd97fd4a1
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2012-02-21 02:14:38 -0200

    Verifies that exports two logs

--- importer/tests/agent_exporter.py
+++ importer/tests/agent_exporter.py
@@ -1,6 +1,6 @@
 import threading
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import mechanize
 
@@ -46,7 +46,53 @@ class ExporterTestCase(NetunomockTestCase, ModelTestCase):
         self.assertTrue(('Date: %s' % start.strftime("%Y%m%d")) in content)
         self.assertTrue('Worked hours: %s' % wp.hours in content)
         self.assertTrue('Description: %s' % get_exported_description(wp) in content)
+
+    def test_export_two_wps(self): 
+        emp = get_employee(get_organization())    
+        agents.importer.import_all(ROOT_URL, 'adam', 'senha')
+
+        e = ImportedEntity.objects.get(category='T', original_id=2376)
+        task = Task.objects.get(id=e.new_id)
+        start = datetime(2011, 12, 31, 8, 0)
+        end = datetime(2011, 12, 31, 12, 0)
+
+        wp1 = WorkingPeriod(employee=emp,
+            intended="Write a function for creating better descriptions",
+            intended_task=task,
+            executed="Created function to describe exported WP",
+            executed_task=task,
+            start= start, end=end)
+        wp1.save()
+
+        delta = timedelta(1)
+        tomorrow_start=start+delta
+        tomorrow_end=end+delta
+        wp2 = WorkingPeriod(employee=emp,
+            intended="Export stuff",
+            intended_task=task,
+            executed="Exporting WPs to old netuno",
+            executed_task=task,
+            start=tomorrow_start, end=tomorrow_end)
+        wp2.save()
+
+
+
+        exporter = agents.Exporter()
+        exporter.export_logs([wp1, wp2], ROOT_URL, 'adam', 'senha')
         
+        browser = mechanize.Browser()
+        response = browser.open(ROOT_URL + SHOW_LOGS_PATH)
+        content = response.read().decode('utf-8')
+        self.assertTrue(('Task id: 2376') in content)
+        self.assertTrue('Log creator: 1' in content)
+        self.assertTrue(('Date: %s' % start.strftime("%Y%m%d")) in content)
+        self.assertTrue('Worked hours: %s' % wp1.hours in content)
+        self.assertTrue('Description: %s' % get_exported_description(wp1) in content)     
+        self.assertTrue(('Task id: 2376') in content)
+        self.assertTrue('Log creator: 1' in content)
+        self.assertTrue(('Date: %s' % tomorrow_start.strftime("%Y%m%d")) in content)
+        self.assertTrue('Worked hours: %s' % wp2.hours in content)
+        self.assertTrue('Description: %s' % get_exported_description(wp2) in content)    
 
     def setUp(self):
         NetunomockTestCase.setUp(self)
