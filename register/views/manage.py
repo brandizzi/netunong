f126ddc698b079ed9e738cbf8d9dae45e43c70ab
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2012-03-28 03:07:52 -0300

    Refactoring: broke big function, addedd docstrings

--- register/views/manage.py
+++ register/views/manage.py
@@ -14,6 +14,9 @@ from register.models import Employee, Task, WorkingPeriod
 from settings import NETUNONG_DATE_FORMAT, NETUNONG_TIME_FORMAT
 
 def get_manage(request):
+    """
+    Shows the working period management page
+    """
     employee = request.user.get_profile()
     filter_params = {}
     startperiod = endperiod = ""
@@ -43,47 +46,63 @@ def get_manage(request):
     return HttpResponse(template.render(context))
 
 def post_manage(request):
-
+    """
+    Decides whether a working period will be updated or deleted. Execute the
+    adequate action.
+    """
     if request.POST.has_key('update'):
-        working_period_id = request.POST['update']
-        working_period = WorkingPeriod.objects.get(id=int(working_period_id))
-        activity = request.POST['activity'+working_period_id]
-        task_id = request.POST['task'+working_period_id]
-        try:
-            task_id = int(task_id)
-            task = Task.objects.get(id=task_id) if task_id > 0 else None
-        except ValueError:
-            task = working_period.last_task
-        start_date = request.POST['start-date'+working_period_id]
-        end_date = request.POST['end-date'+working_period_id]
-        start_time = request.POST['start-time'+working_period_id]
-        end_time = request.POST['end-time'+working_period_id]
-        
-        try:
-            start = datetime.strptime(start_date+" "+start_time,
-                    NETUNONG_DATE_FORMAT+" "+NETUNONG_TIME_FORMAT)
-        except ValueError:
-            start = working_period.start
-        
-        try:
-            end = datetime.strptime(end_date+" "+end_time,
-                    NETUNONG_DATE_FORMAT+" "+NETUNONG_TIME_FORMAT)
-        except ValueError:
-            end = working_period.end
-        
-        working_period.executed = activity
-        working_period.executed_task = task
-        working_period.start = start
-        working_period.end = end
-        working_period.save()
-        # TODO identify the working period
-        messages.success(request, _("The working period was udpated!"))
-    elif  request.POST.has_key('delete'):
-        working_period_id = request.POST['delete']
-        working_period = WorkingPeriod.objects.get(id=int(working_period_id))
-        working_period.delete()
-        # TODO identify the working period
-        messages.success(request, _("The working period was deleted!"))
+        return update_working_period(request)
+    elif request.POST.has_key('delete'):
+        return delete_working_period(request)
+
+def update_working_period(request):
+    """
+    Update data from a working period.
+    """
+    working_period_id = request.POST['update']
+    working_period = WorkingPeriod.objects.get(id=int(working_period_id))
+    activity = request.POST['activity'+working_period_id]
+    task_id = request.POST['task'+working_period_id]
+    try:
+        task_id = int(task_id)
+        task = Task.objects.get(id=task_id) if task_id > 0 else None
+    except ValueError:
+        task = working_period.last_task
+    start_date = request.POST['start-date'+working_period_id]
+    end_date = request.POST['end-date'+working_period_id]
+    start_time = request.POST['start-time'+working_period_id]
+    end_time = request.POST['end-time'+working_period_id]
+    
+    try:
+        start = datetime.strptime(start_date+" "+start_time,
+                NETUNONG_DATE_FORMAT+" "+NETUNONG_TIME_FORMAT)
+    except ValueError:
+        start = working_period.start
+    
+    try:
+        end = datetime.strptime(end_date+" "+end_time,
+                NETUNONG_DATE_FORMAT+" "+NETUNONG_TIME_FORMAT)
+    except ValueError:
+        end = working_period.end
+    
+    working_period.executed = activity
+    working_period.executed_task = task
+    working_period.start = start
+    working_period.end = end
+    working_period.save()
+    # TODO identify the working period
+    messages.success(request, _("The working period was udpated!"))
+    return HttpResponseRedirect(reverse(manage))
+
+def delete_working_period(request):
+    """
+    Delete a working period.
+    """
+    working_period_id = request.POST['delete']
+    working_period = WorkingPeriod.objects.get(id=int(working_period_id))
+    working_period.delete()
+    # TODO identify the working period
+    messages.success(request, _("The working period was deleted!"))
     return HttpResponseRedirect(reverse(manage))
 
 functions = {
